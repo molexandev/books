29fb78fe4012d8798a6dddba4283d38f00a35f56
molexandev01~ <molexandev@gmail.com>
2023-05-30 19:37:45 +0300

    added adaptation few blocks

--- app/js/main.js
+++ app/js/main.js
@@ -1,43 +1,77 @@
-$(function(){
+$(function () {
+    // fadeInUp
 
     (function () {
-      var square = document.querySelector('.animation-block');
-    
-      var observer = new IntersectionObserver(entries => {
-        entries.forEach(entry => {
-          if (typeof getCurrentAnimationPreference === 'function' && !getCurrentAnimationPreference()) {
-            return;
-          }
-    
-          if (entry.isIntersecting) {
-            entry.target.classList.add('animate__fadeInUp');
-          }
+        var square = document.querySelector(".animation-block");
+
+        var observer = new IntersectionObserver((entries) => {
+            entries.forEach((entry) => {
+                if (
+                    typeof getCurrentAnimationPreference === "function" &&
+                    !getCurrentAnimationPreference()
+                ) {
+                    return;
+                }
+
+                if (entry.isIntersecting) {
+                    entry.target.classList.add("animate__fadeInUp");
+                }
+            });
         });
-      });
-    
-      observer.observe(square);
+
+        observer.observe(square);
+    })();
+
+    // img-animated
+
+    (function () {
+        var squareImage = document.querySelector(".about-autor__img");
+        var squareImage2 = document.querySelector(".learning__img");
+
+        var observer = new IntersectionObserver((entries) => {
+            entries.forEach((entry) => {
+                if (
+                    typeof getCurrentAnimationPreference === "function" &&
+                    !getCurrentAnimationPreference()
+                ) {
+                    return;
+                }
+
+                if (entry.isIntersecting) {
+                    entry.target.classList.add("img-animated");
+                }
+            });
+        });
+
+        observer.observe(squareImage);
+        observer.observe(squareImage2);
+    })();
+
+    // fadeIn
+
+    (function () {
+        var fadeIn = document.querySelector(".partners");
+
+        var observer = new IntersectionObserver((entries) => {
+            entries.forEach((entry) => {
+                if (
+                    typeof getCurrentAnimationPreference === "function" &&
+                    !getCurrentAnimationPreference()
+                ) {
+                    return;
+                }
+
+                if (entry.isIntersecting) {
+                    entry.target.classList.add("animate__animated");
+                    entry.target.classList.add("animate__fadeIn");
+                }
+            });
+        });
+
+        observer.observe(fadeIn);
     })();
 
 
-    
-
-      (function () {
-      var squareImage = document.querySelector('.about-autor__img');
-    
-      var observer = new IntersectionObserver(entries => {
-        entries.forEach(entry => {
-          if (typeof getCurrentAnimationPreference === 'function' && !getCurrentAnimationPreference()) {
-            return;
-          }
-    
-          if (entry.isIntersecting) {
-            entry.target.classList.add('img-animated');
-          }
-        });
-      });
-    
-      observer.observe(squareImage);
-    })();
 
 
 
@@ -52,26 +86,43 @@ $(function(){
 
 
 
-   $('.menu__btn').on('click', function () {
-      $('.menu__items').toggleClass('menu__items--active');
-   });
-   
-   $(".response__rating").rateYo({
-      // rating: 3.6,
-      starSvg: '<svg width="13" height="13" viewBox="0 0 13 13" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M6.6666 12.1667C9.88826 12.1667 12.4999 9.55499 12.4999 6.33333C12.4999 3.11167 9.88826 0.5 6.6666 0.5C3.44494 0.5 0.833271 3.11167 0.833271 6.33333C0.833271 9.55499 3.44494 12.1667 6.6666 12.1667Z" stroke="#FFCA42"/ background-color: inherit;></svg>',      
-      normalFill: "transparent",
-      starWidth: "12px",
-      spacing: "3px"
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    $(".menu__btn").on("click", function () {
+        $(".menu__items").toggleClass("menu__items--active");
+    });
+
+    $(".response__rating").rateYo({
+        // rating: 3.6,
+        starSvg:
+            '<svg width="13" height="13" viewBox="0 0 13 13" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M6.6666 12.1667C9.88826 12.1667 12.4999 9.55499 12.4999 6.33333C12.4999 3.11167 9.88826 0.5 6.6666 0.5C3.44494 0.5 0.833271 3.11167 0.833271 6.33333C0.833271 9.55499 3.44494 12.1667 6.6666 12.1667Z" stroke="#FFCA42"/ background-color: inherit;></svg>',
+        normalFill: "transparent",
+        starWidth: "12px",
+        spacing: "3px",
     });
 
     $(".readers__stars").rateYo({
-      // rating: 3.6,
-      starSvg: '<svg width="13" height="13" viewBox="0 0 13 13" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M6.6666 12.1667C9.88826 12.1667 12.4999 9.55499 12.4999 6.33333C12.4999 3.11167 9.88826 0.5 6.6666 0.5C3.44494 0.5 0.833271 3.11167 0.833271 6.33333C0.833271 9.55499 3.44494 12.1667 6.6666 12.1667Z" stroke="#FFCA42"/ background-color: inherit;></svg>',      
-      normalFill: "transparent",
-      starWidth: "20px",
-      spacing: "5px"
+        // rating: 3.6,
+        starSvg:
+            '<svg width="13" height="13" viewBox="0 0 13 13" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M6.6666 12.1667C9.88826 12.1667 12.4999 9.55499 12.4999 6.33333C12.4999 3.11167 9.88826 0.5 6.6666 0.5C3.44494 0.5 0.833271 3.11167 0.833271 6.33333C0.833271 9.55499 3.44494 12.1667 6.6666 12.1667Z" stroke="#FFCA42"/ background-color: inherit;></svg>',
+        normalFill: "transparent",
+        starWidth: "20px",
+        spacing: "5px",
     });
-
-})
-
-
+});
