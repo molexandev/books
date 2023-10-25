eb942904e1633c305885611f22fec99be276f341
molexandev01~ <molexandev@gmail.com>
2023-10-25 18:05:46 +0300

    fix bugs

--- app/js/main.js
+++ app/js/main.js
@@ -6,7 +6,6 @@ $(function () {
         $(this).next().slideToggle();
     });
 
-
     (function () {
         var squareImage = document.querySelector(".about-autor__img");
         var squareImage2 = document.querySelector(".learning__img");
@@ -30,30 +29,32 @@ $(function () {
         observer.observe(squareImage2);
     })();
 
-
     $(".menu__btn, .menu a").on("click", function () {
         $(".menu__items").toggleClass("menu__items--active");
         $(".menu__btn").toggleClass("menu__btn--active");
         $("body").toggleClass("lock");
     });
 
-
-
     $("#menu").on("click", "a", function (e) {
         e.preventDefault();
         var id = $(this).attr("href"),
-            top = $(id).offset().top;
-        $("body,html").animate({ scrollTop: top }, 1500);
+            target = $(id),
+            header = $("#header"), // Замініть на відповідний селектор
+            headerHeight = header.outerHeight(),
+            offset = 10; // Замініть це на бажану величину в пікселях
+
+        if (target.length) {
+            var top = target.offset().top - headerHeight - offset;
+            $("body, html").animate({ scrollTop: top }, 1500);
+        }
     });
 });
 
-wow = new WOW(
-    {
-    boxClass:     'wow',      
-    animateClass: 'animate__animated', 
-    offset:       0,   
-    mobile:       true,
-    live:         true       
-  }
-  )
-  wow.init();
+wow = new WOW({
+    boxClass: "wow",
+    animateClass: "animate__animated",
+    offset: 0,
+    mobile: true,
+    live: true,
+});
+wow.init();
